32f763e8b1aa47dae1af2cc0ef2278538ca175d7
JenSarela <jen.sarela24@gmail.com>
2019-04-04 19:56:04 -0300

    Cifrado listo de mayusculas trabajando en descifrado

--- cipher.js
+++ cipher.js
@@ -1,2 +1,62 @@
-// Inicio de Codificar
-function encode(text)
+window.cipher = {
+   encode:(offset,text) => {
+    console.log(offset,text)
+     let finalResult=""; 
+    /* Acá va tu código */
+        for (let i = 0; i < text.length; i++){ 
+            let positionAscii= text.charCodeAt(i); 
+            console.log(positionAscii);
+           
+          
+            //paso a converti a  codigo ASCII
+          if (positionAscii >= 65 && positionAscii <= 90 ){
+            let cifrado = parseInt((positionAscii-65 + offset)% 26 + 65);
+
+            finalResult += String.fromCharCode(cifrado);
+   
+          }
+          else{
+            finalResult += text[i]
+          }
+          }
+          console.log(finalResult)
+         return finalResult;     
+      
+      }
+    //document.getElementById(finalResult)
+    };
+
+    decode: (offset,text) => {
+      /* Acá va tu código */
+        console.log(offset,text)
+         let finalScore=""; 
+        /* Acá va tu código */
+            for (let i = 0; i < text.length; i++){ 
+                let positionAscii= text.charCodeAt(i); 
+                console.log(positionAscii);
+               
+              
+                //paso a converti a  codigo ASCII
+              if (positionAscii <= 65 && positionAscii >= 90 ){
+                let descifrado = parseInt((positionAscii+ 65 - offset)% 26 + 65);
+    
+                finalScore += String.fromCharCode(descifrado);
+       
+              }
+              else{
+                finalScore += text[i]
+              }
+              }
+              console.log(finalScore)
+             return finalScore;     
+            //document.getElementById(finalScore
+          };
+      
+      
+  
+  
+ 
+
+
+
+
--- index.js
+++ index.js
@@ -1,13 +1,36 @@
-// Funcion para guardar nombre desde el input
-const saveName = () => {
-    const input = document.getElementById("user");
-    nombreDeUsuario = input.value;
-  
-    const nombreUsuario = document.getElementById("nickName");
-    nombreUsuario.innerHTML = nombreDeUsuario;
-    controladorPantallas("comienzo");   
-  }
+/*
+/ Funcion para guardar nombre desde el input
+document.getElementById("nick_name").addEventListener("click", () => {
+ let name = document.getElementById("user").value;
+ document.getElementById("nick").innerHTML = `Hola ${name}!`;
+
+})
+
+//offset 
+let offset = document.getElementById("offset").value;   
+
+//obtener el mensaje a codificar
+let saveTexto = document.getElementById("text").value;
+//llamar al boton para codificar y decodificar usando un evento
+*/
 
 
+document.getElementById("category_codificar").addEventListener("click", () => {
 
- 
+  let text = document.getElementById("text").value;
+  let offset = document.getElementById("offset").value;
+  //let resultado = document.getElementById("answer").value;
+  //if (text != ""){
+    let finalResult = cipher.encode(offset,text);
+    document.getElementById("answer").innerHTML = `${finalResult}`
+  //aqui se deberia mostrar al usuario su nuevo codigo
+  //}
+});
+
+document.getElementById("category_decodificar").addEventListener("click", () =>{
+   let text =document.getElementById("text").value;
+  let offset =document.getElementById("offset").value;
+  let finalScore = cipher.decode(offset,text);
+  document.getElementById("answer").innerHTML = `${finalScore}`
+});
+
